894660741b6bc0c10be3f0f4fdd17b087d51063a
Ubuntu <ubuntu@ip-172-31-8-44.eu-west-1.compute.internal>
2020-10-29 12:45:48 +0000

    safe

--- routes/api.js
+++ routes/api.js
@@ -6,14 +6,16 @@ const Router = express.Router();
 Router.get("/search", (req, res) => {
   const { q } = req.query;
   youtube
-    .search(q || "")
+    .search(
+      q || "", { safeSearch: true }, { safeSearch: true, headers: { Cookie: "PREF=f2=8000000" } }
+    )
     .then((results) =>
       res.send({
         results: results.map((video) => ({ id: video.id, title: video.title })),
         length: results.length,
       })
     )
-    .catch((error) => res.send(error));
+    .catch((error) => [console.error(error), res.send(error)]);
 });
 
 module.exports = Router;
