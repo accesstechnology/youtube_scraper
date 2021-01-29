83297b3b2455192b1721bdf32f16685b69cb9227
Ubuntu <ubuntu@ip-172-31-8-44.eu-west-1.compute.internal>
2021-01-29 13:30:38 +0000

    v3

--- routes/api.js
+++ routes/api.js
@@ -5,25 +5,32 @@ const cache = require("../helpers/cache");
 
 const Router = express.Router();
 
-Router.get("/search", async(req, res) => {
+Router.get("/search", async (req, res) => {
   try {
-    const { q } = req.query;
+    let { q, cached } = req.query;
+    const allowCached = cached !== "false";
     if (!q) return res.send({ error: "Invalid query!" });
-    const cachedResults = await cache.getResults(q);
-    if (cachedResults) return res.send({ results: cachedResults, cached: true });
+    q = q.trim();
+    if (allowCached) {
+      const cachedResults = await cache.getResults(q);
+      if (cachedResults && cachedResults.length !== 0) {
+        return res.send({ results: cachedResults, cached: true });
+      }
+    }
     let freshResults = await youtube.search(
-      q, { safeSearch: true }, { safeSearch: true, headers: { Cookie: "PREF=f2=8000000" } }
+      q,
+      { safeSearch: true },
+      { safeSearch: true, headers: { Cookie: "PREF=f2=8000000" } }
     );
     freshResults = freshResults.map((video) => ({
       id: video.id,
       title: video.title,
     }));
     res.send({ results: freshResults, length: freshResults.length });
-    await cache.saveResults(q, freshResults);
-  }
-  catch (error) {
+    if (freshResults.length > 0) await cache.saveResults(q, freshResults);
+  } catch (error) {
     console.error(error);
-    res.send({ error: "Backend has occured!" });
+    res.send({ error: "Backend error." });
   }
 });
 
